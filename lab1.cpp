f1b33d6baea65fbd84cf29dd683e9b25af6c5d44
fossti <95429787+fossti@users.noreply.github.com>
2024-10-15 21:26:04 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float x, y;
+    cin >> x;
+    cin >> y;
+    double R = 1 / cos(pow(y, 2) / x);
+    double S = pow(x + log(1 / x), 1 / 3);
+    cout << "R = " << R << endl;
+    cout << "S = " << S << endl;
+    double C = max(R, S);
+    cout << C << endl;
+    system("pause");
+    return 0;
+}
